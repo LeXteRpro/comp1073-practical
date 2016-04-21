620e3cb5b790393a43658c4f1afb0f197207a397
Orie <lexter@hotmail.ca>
2016-04-21 11:05:13 -0400

    random numbers

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,15 @@
 
     "use strict";
 
+function diceOneFunction() {
+    document.getElementById("diceNum1").innerHTML =
+    Math.floor(Math.random() * 6) +1;
+}
+
+function diceTwoFunction() {
+    document.getElementById("diceNum2").innerHTML =
+    Math.floor(Math.random() * 6) +1;
+}
+
 
 })();
-
