efe5252902564f6dae5949cf688940eca06a0bac
Orie <lexter@hotmail.ca>
2016-04-21 11:37:04 -0400

    Comments, added variables..

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,11 +3,17 @@
 
     "use strict";
 
+var diceOne = "";
+
+var diceTwo = "";
+
+// Dice1 Onclick function when button is pressed
 function diceOneFunction() {
     document.getElementById("diceNum1").innerHTML =
     Math.floor(Math.random() * 6) +1;
 }
 
+// Dice2 Onclick function when button is pressed
 function diceTwoFunction() {
     document.getElementById("diceNum2").innerHTML =
     Math.floor(Math.random() * 6) +1;
